298ab542806b525e3a72c23f77e20721e564d66e
M Zain Ul Hassan <57948776+MZain-97@users.noreply.github.com>
2022-08-19 15:32:18 +0500

    Delete Chapter01 directory

--- Chapter01/Plot_Sample.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-x = np.linspace(0, 20, 50)
-plt.plot(x, x, label='linear')
-plt.legend()
-plt.show()
--- Chapter01/TensorFlow-Hello.py
+++
@@ -1,5 +0,0 @@
-# Display "Hello, world!"
-import tensorflow as tf
-Message = tf.constant("Hello, world!")
-sess = tf.Session()
-print(sess.run(Message))
